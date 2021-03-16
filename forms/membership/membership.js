c4c1ab993c13879d8fb9415cd3c7299985303679
cjf07630 <79482887+cjf07630@users.noreply.github.com>
2021-03-16 16:58:04 -0500

    Main

---
+++ forms/membership/membership.js
@@ -0,0 +1,9 @@
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+
+btnMember.onclick=function(){
+  if (members.includes(inptMember.value))
+  lblMember.value = "You are already a member"
+  else 
+   lblMember.value = members.push(inptMember.value) && "You are now a member"
+}
